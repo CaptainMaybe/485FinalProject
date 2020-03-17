0bf8ceb3cb3075f033c817e20558767c27530224
Norwick <Jocelyn.KY.Lee@gmail.com>
2020-03-16 19:10:15 -0700

    increased epoch, hidden layer, and decreased range from 0-255 to 0-1 for inputs, ensuring inputs are not whole numbers

--- fashionrecognizer.m
+++ fashionrecognizer.m
@@ -3,10 +3,10 @@ loadthething
 train_num = 50000;
 test_num = 10000;
 
-P = train_data(1:train_num,:);
+P = train_data(1:train_num,:) / 255.0;
 T = train_label(1:train_num,1);
 T = convertT(T);
-test_P = train_data(train_num + 1:end,:);
+test_P = train_data(train_num + 1:end,:) / 255.0;
 test_T = train_label(train_num + 1:end,1);
 test_T = convertT(test_T);
 
