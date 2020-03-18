13e6e8d977f90317ca08ff40fc8a0fb9a83dd3fd
Norwick <Jocelyn.KY.Lee@gmail.com>
2020-03-18 01:30:05 -0700

    finished some backprop trials and saved the matlab figures

--- fashionrecognizer.m
+++ fashionrecognizer.m
@@ -10,7 +10,8 @@ test_P = train_data(train_num + 1:end,:) / 255.0;
 test_T = train_label(train_num + 1:end,1);
 test_T = convertT(test_T);
 
-lr = [0.02, 0.04, 0.06, 0.08, 0.1];
+lr = [0.02 0.04 0.06 0.08 0.1];
+% lr = [0.1, 0.12, 0.14, 0.16, 0.18];
 lr_num = size(lr);
 lr_num = lr_num(2);
 trial_num = 10;
@@ -21,8 +22,8 @@ for ln = 1:lr_num
         total = 0;
 
         for i = 1:test_num
-            inhibP = latInhibSquare(test_P(i,:)');
-            [n1, n2, a, a1] = propogateForward(inhibP, W1, W2, b1, b2);
+            %inhibP = latInhibSquare(test_P(i,:)');
+            [n1, n2, a, a1] = propogateForward(test_P(i,:)', W1, W2, b1, b2);
             e = sum(squaredError(a, test_T(i,:)'));
             total = total + e;
         end
