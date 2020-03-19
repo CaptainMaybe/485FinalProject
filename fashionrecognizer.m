d05b7681010c122ec96d4a070a44b6bd41f4ee0c
Norwick <Jocelyn.KY.Lee@gmail.com>
2020-03-18 18:18:10 -0700

    trying variable learning rate

--- fashionrecognizer.m
+++ fashionrecognizer.m
@@ -10,20 +10,26 @@ test_P = train_data(train_num + 1:end,:) / 255.0;
 test_T = train_label(train_num + 1:end,1);
 test_T = convertT(test_T);
 
-lr = [0.02 0.04 0.06 0.08 0.1];
+%lr = [0.02 0.04 0.06 0.08 0.1];
 %lr = [0.004, 0.008, 0.012, 0.014, 0.016, 0.02];
-lr_num = size(lr);
-lr_num = lr_num(2);
+%lr_num = size(lr);
+%lr_num = lr_num(2);
 hnn = 25;
 %lr = .08;
 %hnn = [10 15 20 25 30 35];
 %hn_num = size(hnn);
 %hn_num = hn_num(2);
 trial_num = 10;
-
-for ln = 1:lr_num
+percent = [1.01 1.02 1.03 1.04 1.05];
+per_num = size(percent);
+per_num = per_num(2);
+down_factor = 0.95;
+up_factor = 1.05;
+lr = 0.2;
+mse = -5;
+for pn = 1:per_num
     for tn = 1:trial_num
-        [W1, W2, b1, b2, out] = backprop(train_num,P, T, lr(ln), hnn);
+        [W1, W2, b1, b2, out] = backprop(train_num,P, T, lr, hnn, percent(pn), down_factor, up_factor);
         total = 0;
         
         for i = 1:test_num
@@ -33,6 +39,6 @@ for ln = 1:lr_num
             e = sum(squaredError(a, test_T(i,:)'));
             total = total + e;
         end
-        e2(ln,tn) = total/test_num;
+        e2(pn,tn) = total/test_num;
     end
 end
--- updateWeightAndBiases.m
+++ updateWeightAndBiases.m
@@ -1,8 +1,9 @@
-function [W_m, b_m] = updateWeightAndBiases(W, b, s, a, r)
+function [W_m, b_m, W_old, b_old] = updateWeightAndBiases(W, b, s, a, r)
 	%Wm(k+1) = Wm(k) - r(sm* aM+1)'
 	%bm(k+1) = bm(k) - r(sm)
 	%r = learning rate
  	%k = ? iteration?
   W_m = W  - r*(s * a');
   b_m = b -r*s;
-  
+  W_old = W;
+  b_old = b;
